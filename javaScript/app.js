9a8f92ed2507dca46c9ae303b764ba51275c6b30
Scott Diallo <diallot@me.com>
2016-06-04 19:20:03 -0700

    sample images added with carousel basic

--- javaScript/app.js
+++ javaScript/app.js
@@ -1,9 +1,47 @@
-$('document').ready(function () {
-    //global variables declarations
+'use strict'
+//global variables declarations
 
-    $('.hero-text').on('click', function () {
-        alert('Welcome to my world!');
-    })
+$('document').ready(function () {
+
+    /*-------------------------------------------------------------------------------
+                                  Carousel / image slider
+      -------------------------------------------------------------------------------*/
+    //settings for slider
+    var width = 720;
+    var animationSpeed = 2000;
+    var pause = 4000;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function () {
+            $slideContainer.animate({
+                'margin-left': '-=' + width
+            }, animationSpeed, function () {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+
 
 
 });
